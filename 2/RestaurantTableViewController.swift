117f0dfaf79a35e7395f5862b8134762a3c3be59
3 <a8570870z@gmail.com>
2020-10-29 11:22:32 +0800

    hjkl

--- 2/RestaurantTableViewController.swift
+++ 2/RestaurantTableViewController.swift
@@ -11,7 +11,57 @@ import UIKit
 class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
 
+    var restaurantIsVisited = Array(repeating: false, count: 21)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //建立一個選單作為動作清單
+        let optionMenu = UIAlertController(title: nil, message:"What do you want to do ?", preferredStyle: .actionSheet)
+        // Add Call action
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+        let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+        alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertMessage, animated: true, completion: nil)
+        }
+        // call-in action
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        
+        // Check-in action
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler: {
+        (action:UIAlertAction!) -> Void in
+        
+        // UndoCheckin action
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.accessoryType = .checkmark
+        self.restaurantIsVisited[indexPath.row] = true
+        })
+        optionMenu.addAction(checkInAction)
+        
+        
+        let UndoCheckInAction = UIAlertAction(title: "Undo Checkin", style: .default, handler: {
+        (action:UIAlertAction!) -> Void in
+        
+        let cell = tableView.cellForRow(at: indexPath)
+            
+        if self.restaurantIsVisited[indexPath.row]{
+            cell?.accessoryType = .none
+            self.restaurantIsVisited[indexPath.row] = false
+            }
+            
+        })
+        optionMenu.addAction(UndoCheckInAction)
+
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel,handler: nil)
+        optionMenu.addAction(cancelAction)
+        present(optionMenu, animated:  true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -40,9 +90,48 @@ class RestaurantTableViewController: UITableViewController {
         // Configure the cell...
         cell.nameLabel?.text = restaurantNames[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
+        if restaurantIsVisited[indexPath.row] {
+        cell.accessoryType = .checkmark
+        } else {
+        cell.accessoryType = .none
+        }
+
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+    {
+    let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+    // Delete the row from the data source
+    self.restaurantNames.remove(at: indexPath.row)
+    self.restaurantLocations.remove(at: indexPath.row)
+    self.restaurantTypes.remove(at: indexPath.row)
+    self.restaurantIsVisited.remove(at: indexPath.row)
+    self.restaurantImages.remove(at: indexPath.row)
+
+    self.tableView.deleteRows(at: [indexPath], with: .fade)
+
+    // Call completion handler with true to indicate
+    completionHandler(true)
+    }
+
+    let shareAction = UIContextualAction(style: .normal, title: "Share") { (action, sourceView, completionHandler) in
+    let defaultText = "Just checking in at " + self.restaurantNames[indexPath.row]
+
+    let activityController = UIActivityViewController(activityItems: [defaultText], applicationActivities: nil)
+
+    self.present(activityController, animated: true, completion: nil)
+    completionHandler(true)
+    }
+
+    let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+
+    return swipeConfiguration
+    }
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
